b78691afaac04fcf0cd41a5983a3ae823cb0b35b
Vic Chimenti <victor.chimenti@gmail.com>
2021-09-07 17:07:18 -0700

    import filter

---
+++ filter.js
@@ -0,0 +1,46 @@
+<script>
+$(function () {
+	$('input#id_search').quicksearch('div.mentorSummaryWrapper', {
+		'delay': 400,
+		'selector': '.mentorSummary',
+		'stripeRows': ['first', 'second', 'third', 'fourth', 'fifth', 'sixth'],
+		'noResults': '.noResultsToShow',
+		'bind': 'keyup click',
+		'minValLength': 2,
+    'prepareQuery': function (val) {
+			return new RegExp(val, "i");
+		},
+		'testQuery': function (query, txt, _row) {
+			return query.test(txt);
+		},
+		'show': function() {
+			$(this).removeClass('hideByTextbox');
+		},
+		'hide': function() {
+			$(this).addClass('hideByTextbox');
+		}
+	});
+});
+
+$(function () {
+	$('#selectbox').quicksearch('div.mentorSummaryWrapper', {
+		'delay': 100,
+		'selector': 'p',
+		'bind': 'change',
+    'prepareQuery': function (val) {
+			return new RegExp(val, "i");
+		},
+		'testQuery': function (query, txt, _row) {
+			return query.test(txt);
+		},
+		'show': function() {
+			$(this).removeClass('hideByDropdown');
+		},
+		'hide': function() {
+			$(this).addClass('hideByDropdown');
+		}
+	});			
+});
+</script>
+
+
