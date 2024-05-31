cd8f1dc22f622b96f51787ca3683cea57f8335a1
Vic Chimenti <victor.chimenti@gmail.com>
2024-05-31 09:33:15 -0700

    import json

---
+++ text-mentor-json.js
@@ -0,0 +1,28 @@
+try {
+    //Defining main functions
+    function processTags(t4Tag) {
+        myContent = content || null;
+        return String(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, myContent, language, isPreview, t4Tag));
+    }
+
+    var photoid = processTags('<t4 type="content" name="Image ID" output="normal" modifiers="striptags,htmlentities" />');
+
+    var list = {};
+    list['contentID'] = processTags('<t4 type="meta" meta="content_id" />');
+    list['firstName'] = processTags('<t4 type="content" name="First Name" output="normal" modifiers="striptags,htmlentities" />');
+    list['lastName'] = processTags('<t4 type="content" name="Last Name" output="normal" modifiers="striptags,htmlentities" />');
+    list['mentorType'] = processTags('<t4 type="content" name="Mentor Type" output="normal" display_field="value" />').replace(/,\s*/g, '|').replace(/\//g, ', ');
+    list['jobTitle'] = processTags('<t4 type="content" name="Job Title" output="normal" modifiers="striptags,htmlentities" />');
+    list['company'] = processTags('<t4 type="content" name="Company" output="normal" modifiers="striptags,htmlentities" />');
+    list['industry'] = processTags('<t4 type="content" name="Industry" output="normal" modifiers="striptags,htmlentities" />');
+    list['mentorSince'] = processTags('<t4 type="content" name="Mentor Since" output="normal" modifiers="striptags,htmlentities" />');
+    list['studentType'] = processTags('<t4 type="content" name="Student Type" output="normal" modifiers="striptags,htmlentities" />').replace(/,\s*/g, '|').replace(/\//g, ', ');
+    list['photoUrl'] = processTags('<t4 type="media" id="' + photoid + '" formatter="path/*" />');
+    list['photoAlt'] = processTags('<t4 type="media" id="' + photoid + '" formatter="image/description" />');
+    list['link'] = processTags('<t4 type="content" name="Name" output="fulltext" use-element="true" filename-element="Name" modifiers="striptags,htmlentities" />');
+
+    var jsonObj = new org.json.JSONObject(list);
+    document.write(jsonObj.toString() + ',');
+} catch (err) {
+    document.write(err);
+}
