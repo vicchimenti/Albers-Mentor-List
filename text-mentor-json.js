dcf2923d337bf4d01eb8cf8850f2b2a25801c278
Vic Chimenti <chimenti@seattleu.edu>
2024-09-10 05:38:16 -0700

    reorder

--- text-mentor-json.js
+++ text-mentor-json.js
@@ -12,11 +12,11 @@ try {
     list['contentID'] = processTags('<t4 type="meta" meta="content_id" />');
     list['firstName'] = processTags('<t4 type="content" name="First Name" output="normal" modifiers="striptags,htmlentities" />');
     list['lastName'] = processTags('<t4 type="content" name="Last Name" output="normal" modifiers="striptags,htmlentities" />');
-    list['mentorType'] = processTags('<t4 type="content" name="Mentor Type" output="normal" display_field="value" />').replace(/,\s*/g, '|').replace(/\//g, ', ');
     list['jobTitle'] = processTags('<t4 type="content" name="Job Title" output="normal" modifiers="striptags,htmlentities" />');
     list['company'] = processTags('<t4 type="content" name="Company" output="normal" modifiers="striptags,htmlentities" />');
     list['industry'] = processTags('<t4 type="content" name="Industry" output="normal" modifiers="striptags,htmlentities" />');
     list['mentorSince'] = processTags('<t4 type="content" name="Mentor Since" output="normal" modifiers="striptags,htmlentities" />');
+    list['mentorType'] = processTags('<t4 type="content" name="Mentor Type" output="normal" display_field="value" />').replace(/,\s*/g, '|').replace(/\//g, ', ');
     list['studentType'] = processTags('<t4 type="content" name="Mentee Type" output="normal" display_field="value" />').replace(/,\s*/g, '|').replace(/\//g, ', ');
     list['photoUrl'] = processTags('<t4 type="media" id="' + photoid + '" formatter="path/*" />');
     list['photoAlt'] = processTags('<t4 type="media" id="' + photoid + '" formatter="image/description" />');
